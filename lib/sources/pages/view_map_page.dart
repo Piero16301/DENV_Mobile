6a86b609b60e61ec68be8fd5804cb53cf24e43dd
Piero Morales <piero.morales@utec.edu.pe>
2021-12-28 20:18:43 -0500

    Se agrega el ícono centrado con cuadro de infromación

--- lib/sources/pages/view_map_page.dart
+++ lib/sources/pages/view_map_page.dart
@@ -12,21 +12,40 @@ class ViewMapPage extends StatefulWidget {
 
 class _ViewMapPageState extends State<ViewMapPage> {
   final Completer<GoogleMapController> _controller = Completer();
+
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
   MapType mapType = MapType.normal;
+
+  @override
+  void initState() {
+    super.initState();
+    setMarker();
+  }
+
+  void setMarker() async {
+    markerIcon = await BitmapDescriptor.fromAssetImage(const ImageConfiguration(), 'assets/map_marker.png');
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     const CameraPosition initialPoint = CameraPosition(
         target: LatLng(-12.135163895120733, -77.02331503157205),
-        zoom: 17.5,
-        tilt: 50
+        zoom: 17.5
     );
 
     // Llamar marcadores desde base de datos
     Set<Marker> markers = <Marker>{};
-    markers.add(const Marker(
-      markerId: MarkerId('-MYMR9IcLh9d5A2m4zbn'),
-      position: LatLng(-12.135163895120733, -77.02331503157205),
+
+    markers.add(Marker(
+      markerId: const MarkerId('-MYMR9IcLh9d5A2m4zbn'),
+      icon: markerIcon,
+      anchor: const Offset(0.5, 0.5),
+      position: const LatLng(-12.135163895120733, -77.02331503157205),
+      infoWindow: const InfoWindow(
+        title: 'Mosquitos',
+        snippet: 'Se han reportado mosquitos bastantes',
+      ),
     ));
 
     return Scaffold(
