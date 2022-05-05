77cd6f466dcea978b61b9a1118b1a201c986dd5b
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2022-05-04 22:37:46 -0500

    Reinicio de proyecto
    Construcción desde cero.

--- android/app/src/main/kotlin/com/deteccion/dengue/utec/deteccion_zonas_dengue/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.deteccion.dengue.utec.deteccion_zonas_dengue
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/deteccion/dengue/utec/deteccion_zonas_dengue/deteccion_zonas_dengue_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deteccion.dengue.utec.deteccion_zonas_dengue.deteccion_zonas_dengue_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
