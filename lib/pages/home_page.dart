baef86acb02009ba8fa1e3f2093e2ef355be1eed
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2022-08-02 23:48:26 -0500

    Se agrega texto de ubicación actual

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,9 +19,65 @@ class HomePage extends StatelessWidget {
     }
 
     return Scaffold(
-      body: Center(
-        child: Text('${locationProvider.currentAddress!.formattedAddress}'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    MapButton(),
+                  ],
+                ),
+              ),
+              const Text(
+                'Ubicación actual',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '${locationProvider.currentAddress!.formattedAddress}',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class MapButton extends StatefulWidget {
+  const MapButton({Key? key}) : super(key: key);
+
+  @override
+  State<MapButton> createState() => _MapButtonState();
+}
+
+class _MapButtonState extends State<MapButton> {
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        if (isLoading) return;
+
+        setState(() => isLoading = true);
+        await Future.delayed(const Duration(seconds: 3));
+        setState(() => isLoading = false);
+      },
+      child: isLoading
+          ? const CircularProgressIndicator(
+              color: Colors.white,
+            )
+          : const Text('Mostrar mapa'),
     );
   }
 }
